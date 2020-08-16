b14bdb9dc5316cab4a5972efde55e4edcd7091ca
Abdallah <abdallah@MacBook-Air-de-Abdallah.local>
2020-08-16 19:10:08 +0200

    initial commit

---
+++ src/robots.js
@@ -0,0 +1,60 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz'
+
+    },
+    {
+        id: 2,
+        name: 'Isaac Hendi',
+        username: 'Hend',
+        email: 'bringer@gmail.com'
+
+    },
+    {
+        id: 3,
+        name: 'Sara Paradise',
+        username: 'Beauty',
+        email: 'power@hotmail.fr'
+
+    },
+    {
+        id: 4,
+        name: 'Isa Toure',
+        username: 'Tower',
+        email: 'kenz@free.biz'
+
+    },
+    {
+        id: 5,
+        name: 'Warda Bouras',
+        username: 'The Ras',
+        email: 'notfunny@lol.biz'
+
+    },
+    {
+        id: 6,
+        name: 'Bryan Macnight',
+        username: 'Smooth',
+        email: 'banga@bingo.biz'
+
+    },
+    {
+        id: 7,
+        name: 'Awa Bennamou',
+        username: 'first Lady',
+        email: 'ladydi@wanadoo.biz'
+
+    },
+    {
+        id: 8,
+        name: 'Jason Wilson',
+        username: 'Will son',
+        email: 'bg@first.biz'
+
+    }
+     
+
+]
